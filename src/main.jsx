eeb4aed6f0de990193197f87082fa76412b133c5
Dwayne <dune.njenga@student.moringaschool.com>
2025-04-19 20:30:02 +0300

    Added base component and Page files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import routes from "../routes";
+import App from "./app";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(routes)
+const root = document.getElementById("root");
+
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router}/>
+);
